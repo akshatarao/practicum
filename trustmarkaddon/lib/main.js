afb69edd80f30686f36cbf32015aa9264c7f8e94
akshatarao <akshatakrao@gatech.edu>
2014-09-14 15:41:13 -0400

    Iterate through JSON files

--- trustmarkaddon/lib/main.js
+++ trustmarkaddon/lib/main.js
@@ -17,27 +17,47 @@ var button = ToggleButton({
   onClick: displayTrustmarkPanel
 });
 
-
+/**
+ *Display trustmarks in the panel
+ */
 function displayTrustmarkPanel(state)
 {
 	if(state.checked)
 	{
+		
+		//Dummy code
 		var jsondata = self.data.load("trial2.json");
 		jsonObj = JSON.parse(jsondata);
 		console.log(jsonObj.name);
 		panelviewer.displayTrustmarks(button);
+
+		//Get tab URL
+		//Check if it exists in recipients
+		//Get recipient ID
+		//Get trustmark IDs for that recipient
+		//Get trustmark name per recipient
 	}
 }
 
+/**
+ *Get database name
+ */
+function getDBName()
+{
+	return "trustmarkDB";
+}
+
+/**
+ * Initialized Database
+ * Tables - Recipients
+ **/
 function initDB()
 {
-	const dbName = "trustmarkDB";
-
+	const dbName = getDBName();
 	var request = indexedDB.open(dbName,2);
 	
-
 	request.onerror = function(event) {
-		console.log("Errors in db access: " + event.target.errorCode);
+		console.log("An error occurred while accessing trustmark DB:" + event.target.errorCode);
 	};
 
 	request.onsuccess = function(event) {
@@ -50,7 +70,8 @@ function initDB()
 		console.log("DB: " + db);
 		var objectStore = db.createObjectStore("recipients", { keyPath: "identifier" });
 		objectStore.createIndex("name", "name", {unique:true});
-		
+	
+		//Insert dummy data	
 		objectStore.transaction.oncomplete = function(event)	
 		{
 			var recipientObjectStore = db.transaction("recipients", "readwrite").objectStore("recipients");
@@ -62,6 +83,10 @@ function initDB()
 
 }
 
+
+/**
+ * Load trustmarks in cache
+ */
 function loadTrustmarksInCache()
 {
 
@@ -82,6 +107,7 @@ function loadTrustmarksInCache()
 		console.log("Successfully got a connection to database");
 		db = event.target.result;
 
+		//Access dummy data inserted earlier
 		var objectStore = db.transaction("recipients", "readwrite").objectStore("recipients");
 		var fbrequest = objectStore.get("www.facebook.com");
 		
@@ -98,25 +124,35 @@ function loadTrustmarksInCache()
 
 	}
 
+	//Read through files and insert into database
 	var trustmarkjson = self.data.load("trial2.json");
 	jsonObj = JSON.parse(trustmarkjson);
 	console.log(jsonObj.Trustmark.Identifier);
-
-	
-//	var transaction = db.transaction("recipients", "readwrite");
-//	var objectStore = transaction.objectStore("recipients");
-//	var request = objectStore.get("www.facebook.com");
-
-/*	request.onerror = function(event) {
-		console.log("Error accessing facebook data");
-	};
-
-
-	request.onsuccess = function(event) {
-
-	console.log("Recipient Name: " + request.result.name);
-	};*/
 }
 
+function isEmpty(str) 
+{
+    return (!str || 0 === str.length);
+}
+function getDefaultTrustmarks()
+{
+	var configFile = self.data.load("defaulttrustmarks/configfile");
+	var trustmarks = configFile.split("\n");
+	
+	for( var i in trustmarks)
+	{
+		if(!isEmpty(trustmarks[i]))
+		{
+			var trustmarkjson = self.data.load(trustmarks[i]);
+			var jsonObj = JSON.parse(trustmarkjson);
+			console.log(jsonObj.Trustmark.Recipient.Identifier);	
+			console.log(jsonObj.Trustmark.TrustmarkDefinitionReference.Identifier);
+		}
+	}
+
+	
+	//TODO: If does not exist, skip
+}
 initDB();
 loadTrustmarksInCache();
+getDefaultTrustmarks();
